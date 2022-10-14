f39a642cfa34acd3ef56cad1dd67e87f4f6b312e
Gregory Ross <gregross.dev@gmail.com>
2022-10-14 14:06:57 -0400

    update js docs

--- 00-expressions/src/expressions.js
+++ 00-expressions/src/expressions.js
@@ -1,12 +1,12 @@
 // TODO: NUMBERS
 
-/*
+/**
  * sumDouble(1, 2) → 3
  * sumDouble(3, 2) → 5
  * sumDouble(2, 2) → 8
  *
- * @param a
- * @param b
+ * @param {Number} a
+ * @param {Number} b
  * @return int
  *      if params are equal double the sum
  *      else sum
