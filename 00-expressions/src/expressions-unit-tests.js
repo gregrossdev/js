d99d99970a0f860011f2be3ca3425b7b74cf9460
Gregory Ross <gregross.dev@gmail.com>
2022-10-14 14:31:45 -0400

    NUMBERS diff21()

--- 00-expressions/src/expressions-unit-tests.js
+++ 00-expressions/src/expressions-unit-tests.js
@@ -1,15 +1,36 @@
 let should = chai.should();
 
 describe('Expressions', () => {
-    describe("sumDouble()", () => {
-        it("(1, 2) should equal 3", () => {
-            sumDouble(1, 2).should.equal(3);
+    describe('Numbers', () => {
+        describe("sumDouble()", () => {
+            it("(1, 2) should equal 3", () => {
+                sumDouble(1, 2).should.equal(3);
+            });
+            it("(3, 2) should equal 5", () => {
+                sumDouble(3, 2).should.equal(5);
+            });
+            it("(2, 2) should equal 8", () => {
+                sumDouble(2, 2).should.equal(8);
+            });
         });
-        it("(3, 2) should equal 5", () => {
-            sumDouble(3, 2).should.equal(5);
-        });
-        it("(2, 2) should equal 8", () => {
-            sumDouble(2, 2).should.equal(8);
+
+        describe("diff21()", () => {
+            it("(19) should equal 2", () => {
+                diff21(19).should.equal(2);
+            });
+            it("(10) should equal 11", () => {
+                diff21(10).should.equal(11);
+            });
+            it("(21) should equal 0", () => {
+                diff21(21).should.equal(0);
+            });
+            it("(22) should equal 2", () => {
+                diff21(22).should.equal(2);
+            });
+            it("(-10) should equal 31", () => {
+                diff21(-10).should.equal(31);
+            });
         });
     });
+
 });
--- 00-expressions/src/expressions.js
+++ 00-expressions/src/expressions.js
@@ -18,3 +18,23 @@ function sumDouble(a, b)
     if (a == b) return sum * 2;
     else return sum;
 }
+
+/**
+ * diff21(19) → 2
+ * diff21(10) → 11
+ * diff21(21) → 0
+ * diff21(22) → 2
+ * diff21(-10) → 31
+ *
+ * @param {Number} n
+ * @return int
+ *      if n is greater than 21 return double the difference between 21 and n
+ *      else the difference between n and 21
+ */
+function diff21(n)
+{
+    if (n > 21)
+        return 2 * (n - 21);
+    else
+        return 21 - n;
+}
